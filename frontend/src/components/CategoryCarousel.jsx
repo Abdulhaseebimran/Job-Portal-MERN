df3cfacd9687d3011729e80db73cfd46fa936a71
Abdulhaseebimran <haseebimran0001@gmail.com>
2025-02-19 23:56:24 +0500

    hero section and category carousel section complete

---
+++ frontend/src/components/CategoryCarousel.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const CategoryCarousel = () => {
+    const category = [
+        "Frontend Developer",
+        "Backend Developer",
+        "Data Science",
+        "Graphic Designer",
+        "FullStack Developer",
+        "UI/UX Designer",
+        "DevOps Engineer",
+        "Cybersecurity Specialist",
+    ];
+    return (
+        <div className="w-full max-w-4xl mx-auto my-20 px-4">
+            <Carousel className="relative w-full">
+                <CarouselContent className="flex gap-4">
+                    {category.map((cat, index) => (
+                        <CarouselItem
+                            key={index}
+                            className="flex justify-center snap-center md:basis-1/2 lg:basis-1/3"
+                        >
+                            <Button variant="outline" className="rounded-full px-6 py-3 text-lg">
+                                {cat}
+                            </Button>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </div>
+    )
+}
+
+export default CategoryCarousel
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
+import CategoryCarousel from './CategoryCarousel'
 
 const Home = () => {
   return (
     <>
-    <div>
+      <div>
         <Navbar />
-    </div>
+        <HeroSection />
+        <CategoryCarousel />
+      </div>
     </>
   )
 }
